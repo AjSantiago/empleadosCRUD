85969513bd5fb33f033effd317e1bb9196db8587
AjSantiago <isc_aj.santiago@hotmail.com>
2021-12-22 00:26:53 -0600

    Maquetando y preparando el paso de datos en componentes List y Edit

--- src/app/pages/employees/edit/edit.component.ts
+++ src/app/pages/employees/edit/edit.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-edit',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.scss']
+  styleUrls: ['./edit.component.scss'],
 })
 export class EditComponent implements OnInit {
+  valor: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as { value: string };
+    this.valor = state.value;
   }
 
+  ngOnInit(): void {}
 }
--- src/app/pages/employees/list/list.component.ts
+++ src/app/pages/employees/list/list.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationExtras, Router } from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.scss']
+  styleUrls: ['./list.component.scss'],
 })
 export class ListComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onGoToEdit(item: any): void {
+    this.router.navigate(['edit'], { state: { value: item } });
   }
-
+  onGoToSee(item: any): void {
+    this.router.navigate(['details'], { state: { value: item } });
+  }
+  onGoToDelete(item: any): void {
+    alert('Deleted');
+  }
 }
