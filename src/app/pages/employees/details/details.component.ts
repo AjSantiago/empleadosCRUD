f3bf84ef99bc962e01af04cb9a3f04d4d33f0fb8
AjSantiago <isc_aj.santiago@hotmail.com>
2021-12-22 18:32:33 -0600

    Maquetando componente Details

--- src/app/pages/employees/details/details.component.ts
+++ src/app/pages/employees/details/details.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.scss']
+  styleUrls: ['./details.component.scss'],
 })
 export class DetailsComponent implements OnInit {
+  employee: any = null;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as { value: string };
+    this.employee = state.value;
   }
 
+  ngOnInit(): void {}
+
+  onGoToEdit(): void {
+    this.router.navigate(['edit'], { state: { value: this.employee } });
+  }
+
+  onDelete(): void {
+    alert('Deleted');
+  }
+
+  onGoBackToList(): void {
+    this.router.navigate(['list']);
+  }
 }
